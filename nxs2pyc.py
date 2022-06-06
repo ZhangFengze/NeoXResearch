0120bbac661cc28d30f168bce631a327d053f247
CrazyZ <CrazyZZZZ@ZZZZZ>
2022-06-06 21:23:12 +0800

    nxs2pyc.py rotor.py

---
+++ nxs2pyc.py
@@ -0,0 +1,39 @@
+import sys
+import zlib
+
+
+def init_rotor():
+    asdf_dn = 'j2h56ogodh3se'
+    asdf_dt = '=dziaq.'
+    asdf_df = '|os=5v7!"-234'
+    asdf_tm = asdf_dn * 4 + (asdf_dt + asdf_dn + asdf_df) * 5 + \
+        '!' + '#' + asdf_dt * 7 + asdf_df * 2 + '*' + '&' + "'"
+    import rotor
+    rot = rotor.newrotor(asdf_tm)
+    return rot
+
+
+def _reverse_string(s):
+    l = list(s)
+    l = map(lambda x: chr(ord(x) ^ 154), l[0:128]) + l[128:]
+    l.reverse()
+    return ''.join(l)
+
+
+def main():
+    infile = sys.argv[1]
+    outfile = sys.argv[2]
+
+    data = open(infile,"rb").read()
+    rotor = init_rotor()
+    data = rotor.decrypt(data)
+    with open("tmp","wb") as out:
+        out.write(data)
+    data = zlib.decompress(data)
+    data = _reverse_string(data)
+
+    with open(outfile, "wb") as out:
+        out.write(data)
+
+
+main()
