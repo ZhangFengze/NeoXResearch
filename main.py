0fdda12f845d1c348ff93d8a520401c0a9a9d0b0
CrazyZ <CrazyZZZZ@ZZZZZ>
2022-06-07 00:21:16 +0800

    print file name

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
         data = decrypt_buffer.Decrypt(data)
         data = nxs2pyc.nxs2pyc(data)
         filename, data = decrypt_pyc.decrypt_pyc(data)
+        print(filename)
         data = decompile_pyc.decompile_pyc(data)
         with open(sys.argv[2], "wb") as outfile:
             outfile.write(data)
